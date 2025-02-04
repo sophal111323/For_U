372211cb84a6d5ccda9269bbc1da87489bfd7f2f
sophal111323 <phaltp1@gmail.com>
2025-02-04 22:46:07 +0700

    script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ let noButtonSize = 1;
 let yesButtonSize = 1;
 
 yesBtn.addEventListener("click", () => {
-  title.innerHTML = "Yay! I Love You!! 💗";
+  title.innerHTML = "I Love You Too Babe💗";
   btnContainer.classList.add("hidden");
   changeImage("yes");
 });
